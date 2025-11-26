7e22c68f67fd5c2a9e77656c44cb1f66ce332c85
octavio chavarria <octavioveintiocho28@gmail.com>
2025-11-26 08:47:02 -0500

    no se si cambie algo pero posiacaso

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const API_URL =
   (import.meta.env.VITE_API_URL || "http://localhost:3000/api") +
   "/censistas";
 
-//console.log("API_URL RESUELTA =>", API_URL);
+console.log("API_URL RESUELTA =>", API_URL);
 
 
 
